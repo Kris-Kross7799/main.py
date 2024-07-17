491074d6ee2117ded5488e083c7b96c55eefa855
Kris-Kross7799 <shagorenko@mail.ru>
2024-07-18 00:51:28 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,9 @@
+number_of_completed_tasks=('14')
+number_of_hours_spent="1.5"
+Name_of_Course="Pyton"
+Time_for_one_task=0.125
+
+print("Курс:",Name_of_Course,", всего задач:",number_of_completed_tasks,", затрачено часов: ",number_of_hours_spent,", среднее время выполнения -", Time_for_one_task,"часа."
+)
+
+
