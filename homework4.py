a944ff5297d659deea42c422f7ea5fa79ac45e47
Kris-Kross7799 <shagorenko@mail.ru>
2024-07-22 18:43:11 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,13 @@
+#date_of_birth=input("Введите свой год рождения ")
+#print("Здравствуйте, ", name)
+#print(type(name))
+#curret_year=2024
+#age=curret_year-int(date_of_birth)
+
+my_string=input()
+print(len(my_string))
+print((my_string).upper())
+print((my_string).lower())
+print((my_string).replace(" ", ""))
+print((my_string)[0])
+print((my_string)[-1])
