95d7f0160dc12b943a7913e32cd225b1cc1b9d01
Kris-Kross7799 <shagorenko@mail.ru>
2024-07-22 20:36:52 +0300

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,39 @@
+
+
+immutable_var=(1,2,3,4,5, True, 'string', 'coconut')
+print(immutable_var, type(immutable_var))
+#immutable_var[5]='TTT'
+#immutable_var.append('Plumber')
+#print(immutable_var)
+
+mutable_list=[1,2,3,4,5, True, 'string', 'coconut']
+print(mutable_list, type(mutable_list))
+mutable_list[5]='TTT'
+mutable_list.append('Plumber')
+print(mutable_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
