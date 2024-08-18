00c456e0d87bc439e7ca525a19db387201cf9255
Kris-Kross7799 <shagorenko@mail.ru>
2024-08-18 23:49:44 +0300

    Add files via upload

--- Module_3_4.py
+++ Module_3_4.py
@@ -1,10 +1,14 @@
-
 def single_root_words(root_word, *other_words):
     same_words = []
     for i in range(len(other_words)):
-        if root_word in other_words[i]:
-            same_words.append(other_words[i])
-    print(same_words)
+        b = root_word.lower()
+        a = other_words[i].lower()
+        if b in a:
+            same_words.append(a)
+    return (same_words)
 
 
-single_root_words('луг', 'луга', 'луговой', "пенный","душистый", 'луговый')
+rezult1 = single_root_words('лУг', 'лУга', 'луговОй', "пенный", "душистый", 'луГовый')
+rezult2 = single_root_words('Шаг', 'ШагоМер', 'шаговой', "идущий", "Стремительный", 'Быстрый')
+print(rezult1)
+print(rezult2)
