0babb6c60b9a3059b1644f71f23c13ff8be1d341
Kris-Kross7799 <shagorenko@mail.ru>
2024-08-24 00:11:28 +0300

    Add files via upload

---
+++ True_math.py
@@ -0,0 +1,14 @@
+from math import inf
+
+
+def divide(first, second):
+    if second==0:
+        return(inf)
+    else:
+        a=first/second
+        return a
+
+
+
+# divide(49,7)
+# divide(15,0)
