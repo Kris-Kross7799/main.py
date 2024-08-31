6782e86c6f334dc527234eeb01ba0af1bfe541c5
Kris-Kross7799 <shagorenko@mail.ru>
2024-08-31 18:58:22 +0300

    Add files via upload

--- Module_5_2.py
+++ Module_5_2.py
@@ -1,21 +1,3 @@
-#
-# class House:
-#     def __init__(self, name, number_of_floors):
-#         self.name = name
-#         self.number_of_floors = number_of_floors
-#         # self.go_to(new_floor)
-#
-#     def go_to(self, new_floor):
-#         for i in range(1,self.number_of_floors+1):
-#             if new_floor > self.number_of_floors or new_floor < 1:
-#                 print('Такого этажа не существует')
-#                 break
-#             if i == new_floor:
-#                 print(f"Добро пожаловать на {new_floor} этаж!")
-#             elif i < new_floor:
-#                 print(i)
-
-
 class House:
     def __init__(self, name, number_of_floors):
         self.name = name
