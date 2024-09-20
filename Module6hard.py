b937b0d93cf70d12dcc03daad53a67bec6fdd944
Kris-Kross7799 <shagorenko@mail.ru>
2024-09-20 22:49:52 +0300

    Add files via upload

--- Module6hard.py
+++ Module6hard.py
@@ -1,45 +1,44 @@
 class Figure:
-    sides_count = 3
+    sides_count = 0
 
-    def __init__(self, color: [], *sides: []):
-        self.__color = color
-        if len(sides) == self.sides_count:
-            self.__sides = sides
+    def __init__(self, color: list, *sides: list):
+        self.__color=list(color)
+        for i in sides:
+            if len(sides) == self.sides_count:  # проверка длины списка sides и количества сторон фигуры
+                self.__sides = sides
+            else:
+                self.__sides = [1] * self.sides_count
+            self.filled = True
+
+    def __is_valid_color(self, *colors):
+        for i in colors:
+            if len(colors) == 3 and 0 <= isinstance(i, int) <= 255:
+                return True
         else:
-            self.__sides = [sides[0]] * self.sides_count
-        self.filled = True
+            return False
+
+    def set_color(self, r, g, b: int):
+        if self.__is_valid_color(r, g, b):
+            self.__color = r, g, b
 
     def get_color(self):
         return self.__color
 
-    def __is_valid_color(self, r, g, b):
-        self.r = int(r)
-        self.g = int(g)
-        self.b = int(b)
-        if 0 < r <= 255 and 0 < g <= 255 and 0 < b <= 255:
-            return r, g, b
-
-    def set_color(self, r, g, b: int):
-        if self.__is_valid_color(r, g, b):
-            self.__color = [self.r, self.g, self.b]
-            return self.__color
-
-    def is_valid_sizes(self, *new_sides):
+    def __is_valid_sides(self, *new_sides):
         for i in new_sides:
-            if isinstance(i, int) and i > 0 and len(new_sides) == self.sides_count:
-                return True
-            else:
+            if not isinstance(i, int) or i <= 0 or len(new_sides) != self.sides_count:
                 return False
+        return True
 
     def get_sides(self):
         return (self.__sides)
 
-    def __len__(self):
-            return sum(self.__sides)
-
     def set_sides(self, *new_sides):
-            if self.is_valid_sizes(*new_sides):
-                self.__sides = list(new_sides)
+        if self.__is_valid_sides(*new_sides):
+            self.__sides = list(new_sides)
+
+    def __len__(self):
+        return sum(self.__sides)
 
 
 class Circle(Figure):
@@ -54,7 +53,7 @@ class Circle(Figure):
         self.radius = self.__sides[0] / (2 * 3.14)
 
     def get_square(self):
-        return 3.14 * (self.r ** 2)              #площадь круга
+        return 3.14 * (self.r ** 2)  # площадь круга
 
 
 class Triangle(Figure):
@@ -73,9 +72,12 @@ class Cube(Figure):
     def __init__(self, color, *sides):
         super().__init__(color, *sides)
         self.__sides = [sides[0]] * self.sides_count
+        self.Filled = True
 
     def get_volume(self):
-        return self.__sides[0] ** 3
+        return self.__sides[0] ** 3  # площадь куба
+
+
 
 # Проверка кода
 circle1 = Circle((200, 200, 100), 10)
@@ -98,4 +100,3 @@ print(len(circle1))
 
 # Проверка объёма (куба):
 print(cube1.get_volume())
-
