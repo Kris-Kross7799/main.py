4001d158c10e44527cc7c3001795cfbf93e17ac4
Kris-Kross7799 <shagorenko@mail.ru>
2024-09-26 23:09:40 +0300

    Add files via upload

--- файлы в операционной системе.py
+++ файлы в операционной системе.py
@@ -1,23 +1,24 @@
+
+
 import os
 from datetime import datetime
 
-directory='.'
+os.chdir('.')
+
+directory="."
 # abs_path=os.path.abspath("test_file.txt")
 # print("абсолютный путь: ", abs_path)
 for root,dirs,files in os.walk(directory):
     for file in files:
-        file="test_file.txt"
-        root=os.path.dirname(os.path.abspath(file))
         filepath=os.path.join(root,file)
-        # print("Полный путь к файлу: ", filepath)
-        filetime=datetime.fromtimestamp(os.path.getmtime(file))
+        #получили относительный путь к файлу)
+        filetime=datetime.fromtimestamp(os.path.getmtime(filepath))
         # filetime=datetime.fromtimestamp(filetime_0)
         # print(filetime)
-        filesize=os.path.getsize(file)
+        filesize=os.path.getsize(filepath)
         # print(filesize)
         parent_dir=os.path.dirname(filepath)
         # print(parent_dir)
 
 print(f'Обнаружен файл: {file}, путь: {filepath}, Размер: {filesize} байт, \n'
       f'Время изменения: {filetime}, Родительская директория: {parent_dir}')
-
