e16b77ff9d9b167addeeaef1c0ab16ba2ab94a13
Kris-Kross7799 <shagorenko@mail.ru>
2024-09-27 16:31:51 +0300

    Add files via upload

--- файлы в операционной системе.py
+++ файлы в операционной системе.py
@@ -1,27 +1,4 @@
 
-# print("Текущая директория:",    os.getcwd())
-# if os.path.exists("test"): #если путь содержит директорию"test"
-#     os.chdir("test")      #изменять существуюшую директорию на "test"
-# else:
-#     os.mkdir('test')     #создание папки в текущей директории
-#     os.chdir("test")
-# print("Текущая директория:", os.getcwd())
-# # os.makedirs(r'test1\test2')    #создание вложенных папок
-#
-# print(os.listdir())     #файлы и папки внутри рабочей директории(без вложенных)
-# for i in os.walk("."):
-#     print(i)
-# os.chdir(r'C:\Users\petro\PycharmProjects\pythonProject2')  #меняет текущую директорию
-# print("Текущая директория:", os.getcwd())
-# print(os.listdir())
-# file=[f for f in os.listdir() if os.path.isfile(f)]      #список файлов
-# dirs=[d for d in os.listdir() if os.path.isdir(d)]       #список директорий
-# print(file)                                              #печать файлов
-# print(dirs)                                              #печать директорий
-# os.startfile(file[22])                                   #запуск файла
-# print(os.stat(file[22]))                                 #инфо о файле
-# print(os.stat(file[22]).st_size)                         #инфо о размере 22го файла в списке
-# os.system("pip install random2")
 
 import os
 from datetime import datetime
