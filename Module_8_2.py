edcf5aa624e896302bbc6beadfacc3c396eb6e34
Kris-Kross7799 <shagorenko@mail.ru>
2024-09-29 22:07:58 +0300

    Add files via upload

---
+++ Module_8_2.py
@@ -0,0 +1,55 @@
+
+# def f1(number):
+#    total/number
+#
+# def f2():
+#     print("Какой хороший день!")
+#     summ=0
+#     for i in range(-2,2,1):
+#         try:
+#             summ+=f1(i)
+#             print(summ)
+#         except ZeroDivisionError as exc:
+#             print(f"что-то пошло не так - {exc} но программа жива")
+#     return summ/0
+#
+# try:
+#     total=f2()
+#     print(f'вот ваш результат функции {total}')
+# except NameError as exc:
+#     print(f"внутри f2 что-то не так - {exc} но мы устояли")
+
+def personal_sum(numbers):
+    try:
+        result=0
+        incorrect_data=0
+        for i in numbers:
+            try:
+                result+=i
+            except TypeError:
+                incorrect_data+=1
+                print(f"Некорректный тип данных: {i}")
+        return (result, incorrect_data)
+    except TypeError as exc:
+        print("Некорректный тип данных")
+def calculate_average(numbers):
+    try:
+        numbers=list(numbers)
+        num_count=0
+        for i in numbers:
+            if isinstance(i,int) or isinstance(i, float):
+                num_count+=1
+        sum=personal_sum(numbers)
+        srednee=sum[0]/num_count
+        return srednee
+    except ZeroDivisionError:
+        return 0
+    except TypeError as exc:
+        print("В numbers записан некорректный тип данных")
+
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
+
