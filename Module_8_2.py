a8d29f4d84ba254d856af9fd4d2352ea6022f483
Kris-Kross7799 <shagorenko@mail.ru>
2024-09-29 22:32:19 +0300

    Add files via upload

--- Module_8_2.py
+++ Module_8_2.py
@@ -1,19 +1,15 @@
 def personal_sum(numbers):
-    try:
-        result=0
-        incorrect_data=0
-        for i in numbers:
-            try:
-                result+=i
-            except TypeError:
-                incorrect_data+=1
-                print(f"Некорректный тип данных: {i}")
-        return (result, incorrect_data)
-    except TypeError as exc:
-        print("Некорректный тип данных")
+    result=0
+    incorrect_data=0
+    for i in numbers:
+        try:
+            result+=i
+        except TypeError:
+            incorrect_data+=1
+            print(f"Некорректный тип данных: {i}")
+    return (result, incorrect_data)
 def calculate_average(numbers):
     try:
-        numbers=list(numbers)
         num_count=0
         for i in numbers:
             if isinstance(i,int) or isinstance(i, float):
@@ -23,7 +19,7 @@ def calculate_average(numbers):
         return srednee
     except ZeroDivisionError:
         return 0
-    except TypeError as exc:
+    except TypeError:
         print("В numbers записан некорректный тип данных")
 
 
