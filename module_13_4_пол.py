12f1cae5679bc24d730f26493c1cf75181ed8fb0
Kris-Kross7799 <shagorenko@mail.ru>
2024-11-20 15:19:47 +0300

    Add files via upload

--- module_13_4_пол.py
+++ module_13_4_пол.py
@@ -2,7 +2,7 @@ from aiogram import Bot,Dispatcher, executor,types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
-api="8047255672:AAGCnrRCdzHz8SU9HYc9n40iVhLg17pFwvU"
+api=""
 bot=Bot(token=api)
 dp=Dispatcher(bot, storage=MemoryStorage())
 
