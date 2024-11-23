c9b729c2b8b18a113d689254e1b5104b11618545
Kris-Kross7799 <shagorenko@mail.ru>
2024-11-23 12:44:35 +0300

    Add files via upload

--- module_13_5.py
+++ module_13_5.py
@@ -3,7 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-api="8047255672:AAGCnrRCdzHz8SU9HYc9n40iVhLg17pFwvU"
+api=""
 bot=Bot(token=api)
 dp=Dispatcher(bot, storage=MemoryStorage())
 
