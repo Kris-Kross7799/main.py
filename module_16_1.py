c203ea9f2dce66045f7ba1c177f53e2de14eb309
Kris-Kross7799 <shagorenko@mail.ru>
2024-12-19 13:36:48 +0300

    Add files via upload

--- module_16_1.py
+++ module_16_1.py
@@ -4,19 +4,19 @@ app=FastAPI()
 
 @app.get('/')
 async def get_main_page():
-    return {'message': 'Главная страница!'}
+    return 'Главная страница!'
 
 @app.get('/user/admin')
 async def hello_admin():
-    return {'message': 'Вы вошли, как администратор'}
+    return 'Вы вошли, как администратор'
 
 @app.get("/user/{user_id}")
 async def get_user(user_id: int):
-    return {f"Вы вошли, как пользователь № {user_id}"}
+    return f"Вы вошли, как пользователь № {user_id}"
 
 @app.get("/user")
 async def user_info(username:str="Georg",age:str=37):
-    return {f"message": f"Информация о пользователе: {username}, возраст {age}"}
+    return f"Информация о пользователе: {username}, возраст {age}"
 
 
 # uvicorn module_16_1:app --reload
--- module_16_2.py
+++ module_16_2.py
@@ -5,11 +5,11 @@ app=FastAPI()
 
 @app.get('/')
 async def get_main_page():
-    return {'message': 'Главная страница!'}
+    return 'Главная страница!!!'
 
 @app.get('/user/admin')
 async def hello_admin():
-    return {'message': 'Вы вошли, как администратор'}
+    return 'Вы вошли, как администратор'
 
 @app.get("/user/{user_id}")
 async def get_user(
@@ -17,19 +17,20 @@ async def get_user(
                                      lt=100,
                                      description='Enter User ID',
                                      example=77)]):
-    return {f"Вы вошли, как пользователь № {user_id}"}
+    return f"Вы вошли, как пользователь № {user_id}"
 
 @app.get("/user/{username}/{age}")
 async def user_info(
         username:Annotated[str, Path(min_length=5,
                                      max_length=20,
                                      description="Enter username",
+                                     regex="^[a-zA-Z0-9_-]+$",
                                      example='UrbanUser')],
         age:Annotated[int, Path(gt=18,
                                 lt=120,
-                                description='Enter age',
+                                description='Enter age, 18-120 years',
                                 example=24)]):
-    return {f"message": f"Информация о пользователе: {username}, возраст {age}"}
+    return f"Информация о пользователе: {username}, возраст: {age}"
 
 
 # uvicorn module_16_2:app --reload
